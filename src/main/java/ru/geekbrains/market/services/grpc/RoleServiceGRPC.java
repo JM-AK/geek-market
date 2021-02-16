6461e4b8dbc5a3234ee56d53eb92c1a3b743b5ee
JM-AK <komlevalexandr87@gmail.com>
2021-02-17 01:07:12 +0300

    [grpc] add RoleServiceGRPC

--- src/main/java/ru/geekbrains/market/services/grpc/RoleServiceGRPC.java
+++ src/main/java/ru/geekbrains/market/services/grpc/RoleServiceGRPC.java
@@ -1,4 +1,37 @@
 package ru.geekbrains.market.services.grpc;
 
-public class RoleServiceGRPC{
+import io.grpc.stub.StreamObserver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.geekbrains.market.RoleRequest;
+import ru.geekbrains.market.RoleResponse;
+import ru.geekbrains.market.RoleServiceGrpc;
+import ru.geekbrains.market.entities.Role;
+import ru.geekbrains.market.entities.User;
+import ru.geekbrains.market.services.UserServiceImpl;
+
+import java.util.Collection;
+
+
+public class RoleServiceGRPC extends RoleServiceGrpc.RoleServiceImplBase {
+    private UserServiceImpl userService;
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void listRoles(RoleRequest request, StreamObserver<RoleResponse> responseObserver) {
+        String userName = request.getUsername();
+        User user = userService.findByUserName(userName).orElse(null);
+        if (user == null) {
+            responseObserver.onError(new UsernameNotFoundException("User not found"));
+        }
+        Collection<Role> roles = user.getRoles();
+        for (Role r : roles) {
+            responseObserver.onNext(RoleResponse.newBuilder().setId(r.getId()).setName(r.getName()).build());
+        }
+        responseObserver.onCompleted();
+    }
 }
