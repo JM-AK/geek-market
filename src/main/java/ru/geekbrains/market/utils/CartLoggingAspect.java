89bc06d6119f88168572a4f6ae6a16191189821a
JM-AK <komlevalexandr87@gmail.com>
2021-02-22 22:06:03 +0300

    [aop] aop Cart and CartController

--- src/main/java/ru/geekbrains/market/utils/CartLoggingAspect.java
+++ src/main/java/ru/geekbrains/market/utils/CartLoggingAspect.java
@@ -1,21 +1,33 @@
 package ru.geekbrains.market.utils;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Aspect
 @Component
 public class CartLoggingAspect {
+    private Logger logger = Logger.getLogger(CartLoggingAspect.class.getName());
 
+    //логирование класса Корзина - НЕ РАБОТАЕТ !!!
     @After("execution (public void ru.geekbrains.market.utils.Cart.*(..))")
-    public void afterModifyInCartClass() {
-        System.out.println("Test AOP Cart");
+    public void afterModifyInCartClass(JoinPoint jp) {
+        logger.info(jp.toString());
     }
 
-    @After("execution * (public void ru.geekbrains.market.*.CartController.*(..))")
-    public void afterModifyInCartControllerClass() {
-        System.out.println("Test AOP CartController");
+    //логирование работы с корзиной
+    @After("execution (public void ru.geekbrains.market.*.CartController.*(..))")
+    public void afterModifyInCartControllerClass(JoinPoint jp) {
+        logger.info(jp.toString());
+    }
+
+    //логирование метода добавления товара в корзину
+    @After("execution (public * ru.geekbrains.market.*.CatalogController.*Cart(..))")
+    public void afterModifyInCatalogControllerClassAddToCart(JoinPoint jp) {
+        logger.info(jp.toString());
     }
 
 }
