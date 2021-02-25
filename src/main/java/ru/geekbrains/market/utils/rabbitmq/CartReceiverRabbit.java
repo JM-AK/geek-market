f9bb1f33ee190815a94eaec74e6bf95fd8c8b424
JM-AK <komlevalexandr87@gmail.com>
2021-02-25 15:09:42 +0300

    [rabbit] add RabbitReceiver and Sender

--- src/main/java/ru/geekbrains/market/utils/rabbitmq/CartReceiverRabbit.java
+++ src/main/java/ru/geekbrains/market/utils/rabbitmq/CartReceiverRabbit.java
@@ -1,4 +1,30 @@
 package ru.geekbrains.market.utils.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class CartReceiverRabbit {
+    private final static String QUEUE_NAME_PREFIX = "add_to_cart_product";
+
+    public void receiveProductDto () throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME_PREFIX, false, false, false, null);
+
+        final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String msg = new String(delivery.getBody(), "UTF-8");
+            log.info("Reciver " + msg);
+        };
+
+        channel.basicConsume(QUEUE_NAME_PREFIX, true, deliverCallback, consumerTag -> {});
+    }
+
 }
--- src/main/java/ru/geekbrains/market/utils/rabbitmq/CartSenderRabbit.java
+++ src/main/java/ru/geekbrains/market/utils/rabbitmq/CartSenderRabbit.java
@@ -1,4 +1,27 @@
 package ru.geekbrains.market.utils.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.market.entities.dto.ProductDto;
+
+@Service
+@Slf4j
 public class CartSenderRabbit {
+    private final static String QUEUE_NAME_PREFIX = "add_to_cart_product";
+
+    public void sendProductDto(ProductDto productDto) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME_PREFIX, false, false, false, null);
+
+            channel.basicPublish("", QUEUE_NAME_PREFIX, null, productDto.toString().getBytes());
+            log.info("sent " + productDto.toString());
+        }
+    }
 }
