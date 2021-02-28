ba6aefd7b5acf7fa2682afe8f9d3b017faef9a93
JM-AK <komlevalexandr87@gmail.com>
2021-02-28 21:49:16 +0300

    [tests-springboot] test startpage

--- src/test/java/ru/geekbrains/market/TestSpringBoot.java
+++ src/test/java/ru/geekbrains/market/TestSpringBoot.java
@@ -5,11 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.geekbrains.market.config.thymeleaf.AppConfig;
+import ru.geekbrains.market.config.thymeleaf.CustomAuthenticationSuccessHandler;
+import ru.geekbrains.market.config.thymeleaf.ThymeleafSecurityConfig;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,13 +22,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = BCryptPasswordEncoder.class)
 @ActiveProfiles("thymeleaf")
+@Import({AppConfig.class, ThymeleafSecurityConfig.class})
 public class TestSpringBoot {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Test
     public void tryToStart() throws Exception {
@@ -32,12 +41,5 @@ public class TestSpringBoot {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void correctLogin() throws Exception {
-        mockMvc.perform(formLogin("/authenticateTheUser").user("admin").password("123"))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/"));
-    }
 
 }
